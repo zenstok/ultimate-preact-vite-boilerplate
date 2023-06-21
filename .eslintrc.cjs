741c9f5a4fedfdc709855125ca31ebf81ded7255
zenstok <raressh_27@yahoo.com>
2023-06-21 13:54:13 +0300

    Add prettier + eslint + lintstaged + husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'airbnb',
+        'airbnb-typescript',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'import/no-extraneous-dependencies': ['error', {
+            'devDependencies': true,
+            'optionalDependencies': false,
+            'peerDependencies': false
+        }],
+        'import/prefer-default-export': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'no-param-reassign': 'warn',
+        'react/jsx-props-no-spreading': 'off',
+        'react/require-default-props': 'off',
+        'max-len': [
+            'warn',
+            {
+                code: 140,
+                comments: 140,
+                ignoreComments: false,
+                ignoreTrailingComments: true,
+                ignoreUrls: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true,
+                ignoreRegExpLiterals: true
+            }
+        ],
+        'linebreak-style': ['error', 'unix'],
+        semi: ['error', 'always']
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    }
+};
